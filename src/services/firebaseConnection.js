7783e241bcf2cf0bd513d71dc756f53e6377329b
lucasmaia19 <lucasmaiaalves19@gmail.com>
2021-03-29 18:53:21 -0300

    adicionado contextapi

---
+++ src/services/firebaseConnection.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+let firebaseConfig = {
+  apiKey: "AIzaSyA0wGl9eTrGPM5OPDy1aVhlAjtHlE4Qrrs",
+  authDomain: "sistema-ce77e.firebaseapp.com",
+  projectId: "sistema-ce77e",
+  storageBucket: "sistema-ce77e.appspot.com",
+  messagingSenderId: "918732037562",
+  appId: "1:918732037562:web:b375f9bb86237fd3ef9183",
+  measurementId: "G-XC6D98LVKM"
+};
+
+if(!firebase.apps.length){
+  firebase.initializeApp(firebaseConfig);
+}
+
+export default firebase;
